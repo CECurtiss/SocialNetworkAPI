4aca8ef49b7f40547d7016f4bb92adee998ae27d
CECurtiss <curtissclinton@gmail.com>
2023-01-27 22:03:35 -0600

    created model and schema for User

--- models/User.js
+++ models/User.js
@@ -1,9 +1,38 @@
-const { Schema, Types } = requre('mongoose')
+const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema(
     {
-
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type:Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
     }
 )
 
-module.eports = userSchma;
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
