2a9d4181e877fd1e3921c22c2fc103bb6f30f7da
CECurtiss <curtissclinton@gmail.com>
2023-01-27 22:42:32 -0600

    created model and schema for Thought. added moment to dependencies

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      get: (datevalue) => moment(datevalue).format("h:mm:ss [on] MMM Do YYYY"),
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+        getters: true,
+    }
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
