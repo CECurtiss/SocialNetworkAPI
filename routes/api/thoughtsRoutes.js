bfd1d76285c6aa61afb769985806ff66d7e5758c
CECurtiss <curtissclinton@gmail.com>
2023-01-30 22:46:06 -0600

    finished writing thoughtsRoutes

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { 
+    getAllThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtsController');
+
+router.route('/').get(getAllThoughts).post(createThought)
+
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
