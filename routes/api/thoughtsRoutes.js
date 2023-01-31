bcd7608e357ed8d88de4dc9ff6d8d35c698a03b4
CECurtiss <curtissclinton@gmail.com>
2023-01-30 22:58:14 -0600

    exported routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -14,3 +14,5 @@ router.route('/').get(getAllThoughts).post(createThought)
 router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,10 +15,4 @@ router.route('/:userId').get(getSingleUser).post(updateUser).delete(deleteUser);
 
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
-// get all users
-// get single user by its _id and populated thought and friend data
-// post a new user
-
-// put to update a user by its _id
-// delete to remove a freinds form that users friend list
-
+module.exports = router;
